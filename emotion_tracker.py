1e97fe1a0c7a6fe3ebf91ce49b6aef8a89c0c3fd
ooakind <gywls0724@naver.com>
2022-12-17 16:01:22 +0000

    implement emotion tracker prototype

--- emotion_tracker.py
+++ emotion_tracker.py
@@ -1,15 +1,36 @@
 import json
 from fitbit import Fitbit
+import time
+import logging
+import scipy.stats
+import datetime
+
+MINUTES = 60
+
+logging.basicConfig(filename="test.log", level=logging.DEBUG)
 
 class EmotionTracker:
     def __init__(self):
         self.fitbit = Fitbit()
+        self.time_window = 15
+        self.change_rate_thr = 0.5
+
+        self.date = ""
+        self.last_timestamp = ""
+        self.corr_start_timestamp_list = []
+        self.corr_buffer = []
+        self.last_corr_index = 0
+
+        self.act_calories_dict = {}
+        self.act_distance_dict = {}
+        self.act_steps_dict = {}
+
+        self.time_list = []
+        self.time_int_list = []
         self.hr_list = []
         self.act_calories_list = []
         self.act_distance_list = []
         self.act_steps_list = []
-        self.act_elevation_list = []
-        self.act_floors_list = []
         self.filepath_dict = {
             "hr": "heartrate.json",
             "act_calories": "activity_calories.json",
@@ -19,45 +40,98 @@ class EmotionTracker:
             "act_floors": "activity_floors.json",
         }
 
-    def _reload_fitbit_data(self, date="today"):
-        self.fitbit.get_heartrate(date=date, granularity="1min")
-        self.fitbit.get_activity(date=date, granularity="1min", filepath=self.filepath_dict["act_calories"])
-        self.fitbit.get_activity(date=date, resource="distance", granularity="1min", filepath=self.filepath_dict["act_distance"])
-        self.fitbit.get_activity(date=date, resource="steps", granularity="1min", filepath=self.filepath_dict["act_steps"])
-        self.fitbit.get_activity(date=date, resource="elevation", granularity="1min", filepath=self.filepath_dict["act_elevation"])
-        self.fitbit.get_activity(date=date, resource="floors", granularity="1min", filepath=self.filepath_dict["act_floors"])
+    def _reload_fitbit_data(self, date="today", g_hr="1min", g_act="1min"):
+        self.fitbit.get_heartrate(date=date, granularity=g_hr)
+        self.fitbit.get_activity(date=date, granularity=g_act, filepath=self.filepath_dict["act_calories"])
+        self.fitbit.get_activity(date=date, resource="distance", granularity=g_act, filepath=self.filepath_dict["act_distance"])
+        self.fitbit.get_activity(date=date, resource="steps", granularity=g_act, filepath=self.filepath_dict["act_steps"])
+       
+        _hr_list = []
+        _act_calories_list = []
+        _act_distance_list = []
+        _act_steps_list = []
+        _date = ""
 
         with open(self.filepath_dict["hr"], "r") as file:
             json_file = json.load(file)
-            self.hr_list = json_file["activities-heart-intraday"]["dataset"]
+            _hr_list = json_file["activities-heart-intraday"]["dataset"]
+            _date = json_file["activities-heart"][0]["dateTime"]
         with open(self.filepath_dict["act_calories"], "r") as file:
             json_file = json.load(file)
-            self.act_calories_list = json_file["activities-calories-intraday"]["dataset"]
+            _act_calories_list = json_file["activities-calories-intraday"]["dataset"]
         with open(self.filepath_dict["act_distance"], "r") as file:
             json_file = json.load(file)
-            self.act_distance_list = json_file["activities-distance-intraday"]["dataset"]
+            _act_distance_list = json_file["activities-distance-intraday"]["dataset"]
         with open(self.filepath_dict["act_steps"], "r") as file:
             json_file = json.load(file)
-            self.act_steps_list = json_file["activities-steps-intraday"]["dataset"]
-        with open(self.filepath_dict["act_elevation"], "r") as file:
-            json_file = json.load(file)
-            self.act_elevation_list = json_file["activities-elevation-intraday"]["dataset"]
-        with open(self.filepath_dict["act_floors"], "r") as file:
-            json_file = json.load(file)
-            self.act_floors_list = json_file["activities-floors-intraday"]["dataset"]
+            _act_steps_list = json_file["activities-steps-intraday"]["dataset"]
 
-    def _is_activity(self):
-        pass
+        self.time_list = [item["time"] for item in _hr_list]
+        self.hr_list = [item["value"] for item in _hr_list]
+        self.act_calories_dict = dict([(item["time"], {"level": item["level"], "value": item["value"], "mets": item["mets"]}) for item in _act_calories_list])
+        self.act_distance_dict = dict([(item["time"], item["value"]) for item in _act_distance_list])
+        self.act_steps_dict = dict([(item["time"], item["value"]) for item in _act_steps_list])
+
+        if _date != self.date:
+            self.act_calories_list.clear()
+            self.act_distance_list.clear()
+            self.act_steps_list.clear()
+            self.date = _date
+    
+    def _update_data_cache(self):
+        _new_index = 0
+        if self.last_timestamp != "":
+            _new_index = self.time_list.index(self.last_timestamp) + 1        
+        
+        if len(self.time_list) == _new_index:
+            logging.debug(f'{self.last_timestamp}: fitbit data is pending.')
+            return
+        
+        self.last_timestamp = self.time_list[-1]
+        print(_new_index, len(self.time_list))
+
+        for i in range(_new_index, len(self.time_list)):
+            self.act_calories_list.append(self.act_calories_dict[self.time_list[i]]["value"])
+            self.act_distance_list.append(self.act_distance_dict[self.time_list[i]])
+            self.act_steps_list.append(self.act_steps_dict[self.time_list[i]])
+        
+        for i in range(_new_index, len(self.time_list), self.time_window):
+            start = i
+            end = i + self.time_window
+            if len(self.hr_list[start:end]) < 2:
+                self.last_timestamp = self.time_list[i-1]
+                break
+            _hr_cal_corr = scipy.stats.pearsonr(self.hr_list[start:end], self.act_calories_list[start:end])
+            if _hr_cal_corr.statistic == None:
+                logging.error(f'No correlation value at {self.time_list[i]}.')
+                continue
+            self.corr_buffer.append(_hr_cal_corr.statistic)
+            self.corr_start_timestamp_list.append(self.time_list[i])   
     
     def _check_emotion_peak(self):
-        pass
+        for i in range(self.last_corr_index, len(self.corr_buffer) - 1):
+            _change_rate = self.corr_buffer[i + 1] / self.corr_buffer[i]
+            if _change_rate < self.change_rate_thr:
+                self._send_push(_change_rate, self.corr_start_timestamp_list[i])
+        self.last_corr_index = len(self.corr_buffer) - 1
 
-    def run(self, date="today"):
-        self._reload_fitbit_data(date)
+    def _send_push(self, change_rate, start_timestamp):
+        logging.debug(f'Corr. changed about {change_rate} at {start_timestamp}.')
+
+    def run(self, date="today", g_hr="1min", g_act="1min"):
+        self._reload_fitbit_data(date, g_hr, g_act)
+        self._update_data_cache()
+        self._check_emotion_peak()
+
+    def deploy_run(self, date="today", g_hr="1min", g_act="1min"):
+        while(1):
+            logging.debug(f'Reloading app at {datetime.datetime.now()}.')
+            self.run(date, g_hr, g_act)
+            time.sleep(self.time_window * MINUTES)
 
 if __name__ == "__main__":
     emotion_tracker = EmotionTracker()
-    emotion_tracker.run()
+    emotion_tracker.deploy_run()
         
 
 
