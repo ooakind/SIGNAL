a34778989f414d476758d21d83d93e09c34a2d47
ooakind <gywls0724@naver.com>
2022-12-18 06:13:44 +0000

    set server port to 5000

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def get_emotion_data():
         return jsonify({"error": f'No user information: {user_id}'})
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=5000)
